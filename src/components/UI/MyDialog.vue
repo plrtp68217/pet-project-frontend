5ad032c83da68d8c0afbaa01d83c36a4e88f05d7
”plrtp” <plrtp@mail.com>
2024-08-21 16:30:39 +0300

    added dialog

---
+++ src/components/UI/MyDialog.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="dialog">
+        <div  class="dialog__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'my-dialog',
+    
+   
+}
+</script>
+
+<style scoped>
+    .dialog {
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+
+    .dialog__content {
+        background-color: aliceblue;
+        padding: 10px 30px 10px 30px;
+        border-radius: 18px;
+    }
+</style>
